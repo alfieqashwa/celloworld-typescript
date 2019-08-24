9f22f9d5bd81f1645fd9ed80e4397aefbce84962
alfieqashwa <alfieqashwa@gmail.com>
2019-08-24 17:57:54 +0700

    setting gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Celloworld',
-    description: 'Alfie Qashwa Personal Site to Learn In Public.',
+    description: 'Alfie Qashwa Personal Site to Learn In The Clouds As Dead As Leaves.',
     siteUrl: 'https://www.alfieqashwa.me',
     author: {
       name: 'Alfie Qashwa',
@@ -34,7 +34,67 @@ module.exports = {
         name: `assets`,
       },
     },
-    'gatsby-plugin-mdx',
-    `gatsby-plugin-theme-ui`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // trackingId: '',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-external-links`,
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          // TODO: Replace with "mdx-component-autolink-headers"
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              maintainCase: false,
+            },
+          },
+        ],
+        plugins: [
+          `gatsby-remark-images`,
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://alfieqashwa.me`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Alfie Qashwa`,
+        short_name: `alfieqashwa.me`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/logo.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+    // 'gatsby-plugin-offline',
   ],
 }
