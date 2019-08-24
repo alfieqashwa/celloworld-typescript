dbbd704247ae2f3104eb4053cc91c4a44d49205c
alfieqashwa <alfieqashwa@gmail.com>
2019-08-24 17:29:59 +0700

    installed dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,36 @@
  */
 
 module.exports = {
-  plugins: ['gatsby-plugin-mdx', `gatsby-plugin-theme-ui`],
+  siteMetadata: {
+    title: 'Celloworld',
+    description: 'Alfie Qashwa Personal Site to Learn In Public.',
+    siteUrl: 'https://www.alfieqashwa.me',
+    author: {
+      name: 'Alfie Qashwa',
+      url: 'https://twitter.com/alfieqashwa',
+      email: 'alfieqashwa@gmail.com',
+    },
+    social: {
+      twitter: 'https://twitter.com/alfieqashwa',
+      github: 'https://github.com/alfieqashwa',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    'gatsby-plugin-mdx',
+    `gatsby-plugin-theme-ui`,
+  ],
 }
